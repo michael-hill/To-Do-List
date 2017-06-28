b2c01d53059f2de4a9a8be119d66929338c0e5eb
Michael Hill <michael-hill@live.com>
2017-06-28 14:45:09 -0500

    added css class to refactor click listner

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,4 @@
 // Check off specific todos by clicking
-$("li").click(function(){
-	// if li is gray 
-	if ($(this).css("color") === "gray"){
-		console.log("It is currently gray");
-		// turn it black
-	}
-	// else
-		// turn gray
-	$(this).css({
-		color: "gray",
-		textDecoration: "line-through"
-	});
+$("li").click(function() {
+    $(this).toggleClass("completed");
 });
